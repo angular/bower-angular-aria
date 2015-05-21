2b98dadbe21180fabb118fb33ea4d60f594c671a
jenkins <ci-bot@angularjs.org>
2015-05-21 12:26:34 -0700

    v1.3.16-build.113+sha.3881831

--- angular-aria.js
+++ angular-aria.js
@@ -1,6 +1,6 @@
 /**
- * @license AngularJS v1.4.0-build.4008+sha.3a3db69
- * (c) 2010-2015 Google, Inc. http://angularjs.org
+ * @license AngularJS v1.3.16-build.113+sha.3881831
+ * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
 (function(window, angular, undefined) {'use strict';
@@ -352,8 +352,7 @@ ngAriaModule.directive('ngShow', ['$aria', function($aria) {
 
         if ($aria.config('bindKeypress') && !attr.ngKeypress && !isNodeOneOf(elem, nodeBlackList)) {
           elem.on('keypress', function(event) {
-            var keyCode = event.which || event.keyCode;
-            if (keyCode === 32 || keyCode === 13) {
+            if (event.keyCode === 32 || event.keyCode === 13) {
               scope.$apply(callback);
             }
 
