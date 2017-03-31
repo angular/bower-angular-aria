ad51b1b65c8607d7982e608db7f6efecb66c0f86
jenkins <ci-bot@angularjs.org>
2017-03-31 02:31:25 -0700

    v1.6.4

--- angular-aria.js
+++ angular-aria.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.6.4-build.5351+sha.617b361
+ * @license AngularJS v1.6.4
  * (c) 2010-2017 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -59,7 +59,7 @@
  * {@link guide/accessibility Developer Guide}.
  */
 var ngAriaModule = angular.module('ngAria', ['ng']).
-                        info({ angularVersion: '1.6.4-build.5351+sha.617b361' }).
+                        info({ angularVersion: '1.6.4' }).
                         provider('$aria', $AriaProvider);
 
 /**
