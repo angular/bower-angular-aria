405d8ecfc26882f163ac7072f615af2c1829c8e5
jenkins <ci-bot@angularjs.org>
2018-04-19 02:36:21 -0700

    v1.6.11-build.5554+sha.841feb0

--- angular-aria.js
+++ angular-aria.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.7.0-rc.0
+ * @license AngularJS v1.6.11-build.5554+sha.841feb0
  * (c) 2010-2018 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -57,7 +57,7 @@
  * {@link guide/accessibility Developer Guide}.
  */
 var ngAriaModule = angular.module('ngAria', ['ng']).
-                        info({ angularVersion: '1.7.0-rc.0' }).
+                        info({ angularVersion: '1.6.11-build.5554+sha.841feb0' }).
                         provider('$aria', $AriaProvider);
 
 /**
