cde950bd3ec4d0e02cfde460c0db2b8a4c27824b
jenkins <ci-bot@angularjs.org>
2018-06-12 08:26:41 -0700

    v1.7.2

--- angular-aria.js
+++ angular-aria.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.7.2-build.5558+sha.c9a92fc
+ * @license AngularJS v1.7.2
  * (c) 2010-2018 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -63,7 +63,7 @@
 var ARIA_DISABLE_ATTR = 'ngAriaDisable';
 
 var ngAriaModule = angular.module('ngAria', ['ng']).
-                        info({ angularVersion: '1.7.2-build.5558+sha.c9a92fc' }).
+                        info({ angularVersion: '1.7.2' }).
                         provider('$aria', $AriaProvider);
 
 /**
